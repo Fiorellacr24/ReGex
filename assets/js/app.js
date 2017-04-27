4ad02e937c796aa86b6178b2d826be2f5ac744ae
Fiorella Cisneros <fiorella.0624@gmail.com>
2017-04-27 15:42:23 -0500

    inputs

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+function validarform(){
+	var nombre, apellido, cumpleaños, celular, fijo, contraseña, comentarios, terminos; 
+		nombre = document.getElementById('name').value;
+		apellido = document.getElementById('lastname').value;
+		cumpleaños = document.getElementById('birthday').value;
+		celular = document.getElementById('celphone').value;
+		fijo = document.getElementById('fixed-number').value;
+		contraseña = document.getElementById('password').value;
+		comentarios = document.getElementById('comments').value;
+		terminos = document.getElementById('terms').value;
+}
